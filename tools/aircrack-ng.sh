563effe35d47e00a4f41856cd462f4ec92b96c03
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-20 16:11:04 +0700

    Create aircrack-ng.sh

---
+++ tools/aircrack-ng.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# aircrack-ng wizard
+# by: fii14
+
+read -p "Enter the network interface you want to use: " network_interface
+read -p "Enter the bssid you want to attack: " bssid
+read -p "Enter the path to the capture file: " capture_file
+read -p "Enter the path to the wordlist file: " wordlist_file
+
+aircrack-ng -w "$wordlist_file" --bssid "$bssid" "$capture_file"
