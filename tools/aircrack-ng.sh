905566e3ad446a1516c2a8ec351b8caf61a4ac17
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-20 16:16:11 +0700

    Update aircrack-ng.sh

--- tools/aircrack-ng.sh
+++ tools/aircrack-ng.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-# aircrack-ng wizard
-# by: fii14
+
+# aircrack-ng
+#----------------------------------------------------------------#
+# aircrack-ng is a suite of wireless network security            #
+# tools that is used for auditing the security of Wi-Fi          #
+# networks. It's primarily used to assess the security           #
+# of wireless networks by testing their vulnerabilities          #
+# and evaluating their encryption methods. The suite             #
+# includes several tools that allow you to capture and           #
+# analyze network traffic, perform various attacks on            #
+# wireless networks, and crack WEP and WPA/WPA2 encryption       #
+# keys.                                                          #
+# ---------------------------------------------------------------#
 
 read -p "Enter the network interface you want to use: " network_interface
 read -p "Enter the bssid you want to attack: " bssid
