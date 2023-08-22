b8625418ba82225e78c8a97c98959a36b55d352f
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-22 22:34:09 +0700

    Update aircrack-ng-wizard.sh

--- tools/wireless attack/aircrack-ng-wizard.sh
+++ tools/wireless attack/aircrack-ng-wizard.sh
@@ -7,6 +7,16 @@
 read -p "Enter the network interface you want to use: " network_interface
 read -p "Enter the bssid you want to attack: " bssid
 read -p "Enter the path to the capture file: " capture_file
+
+if [[ ! -f "$capture_file" ]]; then
+    echo "Error: $capture_file file not found."
+    exit 1
+
+elif [[ -f "$capture_file" && "$capture_file##*." != "cap" ]]; then
+    echo "Error: $capture_file file are not capture file."
+    exit 1
+fi    
+    
 read -p "Enter the path to the wordlist file: " wordlist_file
 
 aircrack-ng -w "$wordlist_file" --bssid "$bssid" "$capture_file"
